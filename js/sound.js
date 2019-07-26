982800fc9ba201be8777f843485370a3f3f1645d
Ben Busby <ben.busby7@gmail.com>
2019-07-26 01:01:16 -0600

    Added chrome-specific audio handling

--- js/sound.js
+++ js/sound.js
@@ -1,38 +1,49 @@
 var context;
-var chromeMod = 1;
+var isChrome = 0;
 
 function initAudio() {
     context = new AudioContext();
-    chromeMod = window.chrome ? 10 : 1;
+    isChrome = window.chrome ? 1 : 0;
 }
 
 /**
  * Plays a note using the built in AudioContext api.
  */
-function play(wave, duration, frequency, time, gain) {
+function play(wave, duration, frequency, time, gain, size) {
     var ctxOsc = context.createOscillator();
     var ctxGain = context.createGain();
     ctxOsc.connect(ctxGain);
     ctxOsc.type = wave;
     ctxGain.connect(context.destination);
+
     if (gain) {
         ctxGain.gain.value = gain;
     }
 
-    // Play the note for a specific length of time
-    ctxGain.gain.exponentialRampToValueAtTime(
-        0.00001, context.currentTime + (duration * chromeMod) + time
-    );
     ctxOsc.frequency.value = frequency;
 
-    // Modify start time of note in order to play sequences
-    ctxOsc.start(context.currentTime + (time * chromeMod));
+    // Play the note for a specific length of time
+    if (!isChrome) {
+        ctxGain.gain.exponentialRampToValueAtTime(
+            0.00001, context.currentTime +
+            duration +
+            (time + (isChrome * size))
+        );
+
+        // Modify start time of note in order to play sequences
+        ctxOsc.start(context.currentTime + time);
+    } else {
+        ctxOsc.start(context.currentTime + time);
+        setTimeout(function() {
+            ctxOsc.stop();
+        }, 150 * (size > 1 ? size : 0.25));
+    }
 }
 
 function playSequence(notes) {
     var duration = 0.1;
     for (var i = 0; i < notes.length; i++) {
-        play("sine", duration, notes[i], i * duration, 0.25);
+        play("sine", duration, notes[i], i * duration, 0.25, notes.length);
     }
 }
 
@@ -41,7 +52,7 @@ function playScoreSound() {
 }
 
 function playJumpSound() {
-    play("sine", 0.05, 880.0, 0, 0.5);
+    play("sine", 0.05, 880.0, 0, 0.5, 1);
 }
 
 function playGameOver() {
